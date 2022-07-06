d91a748259d46371b1f7b258f0dca1a69f6df9bc
min33sky <min33sky@naver.com>
2022-07-06 10:46:05 +0900

    :art: test2

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,9 +10,9 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  // pages: {
-  //   signIn: '/auth/signin',
-  // },
+  pages: {
+    signIn: '/auth/signin',
+  },
   callbacks: {
     //? useSession() 훅을 호출할 때 호출되는 콜백 함수
     //? 기본으로 제공되는 정보를 커스터마이징할 수 있다.
